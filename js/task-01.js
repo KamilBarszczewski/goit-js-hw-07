04e361f3766aa7807186d0f7d526492ef512c593
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2023-12-08 21:36:58 +0100

    zadanie 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,12 @@
+const listWithId = document.querySelector('#categories')
+const menuByTagName = listWithId.querySelectorAll(".item")
+console.log("Number of categories: ", menuByTagName.length)
+console.log("")
 
-
-
-const body = document.body;
-
-const list = body.firstElementChild;
-
-const firstListItem = list.firstElementChild;
-const listItems = list.children;
+for (let item of menuByTagName) {
+    const boxTitle = item.querySelector('h2').innerText
+    const boxLength = item.querySelectorAll('li').length
+    console.log(boxTitle)
+    console.log(boxLength)
+    console.log("")
+}
